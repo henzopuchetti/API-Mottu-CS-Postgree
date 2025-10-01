75e644496d5de5f03fbbc3ae90fc5fd1145ae91a
henzo <84385799+henzopuchetti@users.noreply.github.com>
2025-10-01 15:08:12 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using MottuApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowAnyOriginPolicy = "_myAllowAnyOriginPolicy";
 
-// Força escuta na porta 80
-builder.WebHost.UseUrls("http://+:80");
+// Força a aplicação a rodar na porta 8080 (não privilegiada, funciona no ACI sem root)
+builder.WebHost.UseUrls("http://+:8080");
 
 // Configura o DbContext com a string de conexão vinda do ambiente
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DB_CONNECTION") 
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DB_CONNECTION") 
         ?? Environment.GetEnvironmentVariable("DB_CONNECTION")));
 
 // Adiciona suporte a controllers
@@ -21,7 +22,10 @@ builder.Services.AddSwaggerGen(options =>
 {
     var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+    }
 });
 
 // Libera CORS para qualquer origem
